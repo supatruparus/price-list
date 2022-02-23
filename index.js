05d8585ae21d3391280b1cc4599b0d21407299c6
supatruparus <95300675+supatruparus@users.noreply.github.com>
2022-02-24 04:51:32 +0500

    hide form

---
+++ index.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('.form')
+const form_button = document.querySelector('.backcall')
+const showForm = function(){
+  form.style.visibility = 'visible';
+  console.log('click');
+}
+form_button.addEventListener('click', ()=>{
+   showForm()})
+
+document.body.onclick = function(event){
+  // if (getComputedStyle(form).visibility === 'visible') {
+  //   form.style.visibility = 'hidden';
+  // }
+  let bool = event.target === form_button
+  console.log(bool)
+  if(bool != true) form.style.visibility = 'hidden'
+}
